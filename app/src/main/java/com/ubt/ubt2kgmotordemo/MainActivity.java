15cd1fb62b4853da9b974b9a9e7a7c022fcb4c1b
白桦 <afunx.bai@ubtrobot.com>
2017-09-26 11:29:19 +0800

    sdk-v0.6 is ready

---
+++ app/src/main/java/com/ubt/ubt2kgmotordemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.ubt.ubt2kgmotordemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.ubt.ip.sdk.api.MotorApi;
+
+import java.util.Arrays;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        testGetMotors();
+    }
+
+    private void testGetMotors() {
+        new Thread() {
+            @Override
+            public void run() {
+                int[] motorsId = new int[]{1, 2, 3, 4, 5, 6, 7};
+                int[] motorsDegree = new int[motorsId.length];
+                MotorApi.get().getMotors(motorsId, motorsDegree);
+                Log.e(TAG, "motorsId: " + Arrays.toString(motorsId));
+                Log.e(TAG, "motorsDegree: " + Arrays.toString(motorsDegree));
+            }
+        }.start();
+    }
+}
